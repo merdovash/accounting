0e2967089acf43720a697f625e7c60c342b698db
merdovash <merd888888@gmail.com>
2021-05-15 14:46:54 +0300

    edit items

--- lib/registry/pay_registry_item.dart
+++ lib/registry/pay_registry_item.dart
@@ -1,18 +1,38 @@
+import 'package:accounting/forms/PayItemForm.dart';
 import 'package:accounting/registry/registry_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class RegistryPayItem extends StatelessWidget {
+class RegistryPayItem extends StatefulWidget {
   final PayItem payItem;
   RegistryPayItem(this.payItem);
 
   @override
+  _RegistryPayItemState createState() => _RegistryPayItemState();
+}
+
+class _RegistryPayItemState extends State<RegistryPayItem> {
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Text(DateFormat("dd.MM.yy", 'RU-ru').format(payItem.date)),
-      title: Text(payItem.paySum.toString() + '₽'),
+      leading: Text(DateFormat("dd.MM.yy", 'RU-ru').format(widget.payItem.date)),
+      title: Text(widget.payItem.paySum.toString() + '₽'),
+      onTap: () async {
+        PayItem? result = await Navigator.push(
+          context,
+            MaterialPageRoute(builder: (context) {
+              return PayItemForm(
+                this.widget.payItem,
+              );
+            })
+        );
+        if (result != null) {
+          setState(() {
+            //widget.payItem.updateFrom(pa);
+          });
+        }
+      },
     );
   }
-
 }
